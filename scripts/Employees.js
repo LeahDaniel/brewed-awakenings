34795a41c0cadd1a4f4be26283a2c886fc5c43ec
LeahDaniel <leah.daniel1997@gmail.com>
2021-10-15 15:10:50 -0500

    Initial commit- completed tasks

---
+++ scripts/Employees.js
@@ -0,0 +1,40 @@
+import { getEmployees } from "./database.js"
+import { getOrders } from "./database.js"
+
+const employees = getEmployees()
+const orders = getOrders()
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("employee")) {
+            const [,employeeId] = itemClicked.id.split("--")
+            
+            for (const employee of employees) {
+                if (employee.id === parseInt(employeeId)) {
+                    let numberOfOrders = 0
+                    for (const order of orders) {
+                        if (order.employeeId === parseInt(employeeId)) {
+                            numberOfOrders ++
+                        }
+                    }
+                    window.alert(`${employee.name} sold ${numberOfOrders} products`)
+                }
+            }
+        }
+    }
+)
+
+export const Employees = () => {
+    let html = "<ul>"
+
+    for (const employee of employees) {
+        html += `<li id="employee--${employee.id}">${employee.name}</li>`
+    }
+
+    html += "</ul>"
+
+    return html
+}
+
